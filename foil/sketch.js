8bbf180e93246108c4ea572a046912bdb3e834d8
Soobeen <110775069+5oob33n@users.noreply.github.com>
2024-03-05 15:32:36 +0100

    Update sketch.js

--- foil/sketch.js
+++ foil/sketch.js
@@ -46,7 +46,7 @@ function setup() {
   textFont(font);
 
   backButton = createButton('Ignore');
-  backButton.position(40, 455);
+  backButton.position(40, 465);
   backButton.style('background-color', 'transparent'); 
   backButton.style('border', '1px solid #3034ff');  
   backButton.style('color', '#ff0051'); 
