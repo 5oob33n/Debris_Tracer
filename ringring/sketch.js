85c40c5922d2f997e94380931130dbee3337106a
Soobeen <110775069+5oob33n@users.noreply.github.com>
2024-03-05 15:21:35 +0100

    Update sketch.js

--- ringring/sketch.js
+++ ringring/sketch.js
@@ -46,7 +46,7 @@ function setup() {
   textFont(font);
 
   backButton = createButton('Ignore');
-  backButton.position(40, 550);
+  backButton.position(40, 530);
   backButton.style('background-color', 'transparent'); 
   backButton.style('border', '1px solid #3034ff'); 
   backButton.style('color', '#ff0051'); 
